6dd70afd063da875af9439cc97e59bdfc7be348d
Aluno ProWay <=>
2021-08-28 15:25:04 -0300

    Exercicio aula2-1 Resolvido

--- aula2/exercicios/a2-1-Ex1.py
+++ aula2/exercicios/a2-1-Ex1.py
@@ -2,3 +2,18 @@
 # O cadastro deve solicitar ao usuário:
 # Nome, Sobrenome e idade
 # O sistema deve permitir apenas clientes maiores de idade
+# Caso a idade seja menor, informar que nao pode ser cadastrado
+
+
+print('*'*10, 'Cadastro de Clientes', '*'*10)
+print('\n')
+nome = input('Digite o seu nome: ')
+sobrenome = input('Digite seu sobrenome: ')
+idade = int(input('Digite sua idade: '))
+
+if(idade >= 18):
+    print('Cadastrado com sucesso!')
+elif(idade > 0):
+    print('Nao permitido menores de idade')
+else:
+    print('Idade informada é inválida')
