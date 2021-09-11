8e278440991c3bbd8d2b87d8f3bba7aefacdfcbd
Aluno ProWay <aluno@proway.treina>
2021-09-11 14:20:34 -0300

    Aula3 - Procedimentos

---
+++ aula3/a3-1.py
@@ -0,0 +1,34 @@
+# Curso: Python Fundamentos Proway
+# Aula : Dia 3 - Parte 3 - 1
+# Assunto: procedimentos
+# Data: 2021-09-11
+
+# Procedimento = Não possui retorno
+# Função = Existe retorno
+# parametro = é a variavel recebida em uma funcao ou procedimento
+# argumento = é o dado passado durante a chamda de uma funcao ou procedimento
+
+# soma - 
+def somar(numero1, numero2):
+    resultado = numero1 + numero2
+    print(resultado)
+
+#subtracao
+def subt(numero1, numero2):
+    resultado = numero1 - numero2
+    print(resultado)
+
+#multiplicacao
+def mult(numero1, numero2):
+    resultado = numero1 * numero2
+    print(resultado)
+
+#divisao
+def divi(numero1, numero2):
+    resultado = numero1 / numero2
+    print(resultado)
+
+somar(10, 12)
+subt(10, 12)
+mult(10, 12)
+divi(10, 12)
